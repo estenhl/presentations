615a4327bd30082d7ef3ac1f2ba33b3a3bb3bfda
Esten Leonardsen <estenleonardsen@gmail.com>
2024-08-27 14:54:27 +0200

    Working through linear regression for PSY9511

--- 240829-psy9511-linreg/scripts/find_functions.py
+++ 240829-psy9511-linreg/scripts/find_functions.py
@@ -13,9 +13,15 @@ print(f'Linear regression intercept: {model.intercept_}, coef: {model.coef_[0]}'
 
 print(f'Constant intercept: {np.mean(y)}')
 
-df = pd.read_csv(os.path.join('data', 'Auto.csv'))
 X = df[['horsepower']]
 y = df['mpg']
 model = LinearRegression()
 model.fit(X, np.log(y))
 print(f'Exponential intercept: {model.intercept_}, coef: {model.coef_[0]}')
+
+
+X = df[['horsepower', 'weight']]
+y = df['mpg']
+model = LinearRegression()
+model.fit(X, y)
+print(f'Multiple regression intercept: {model.intercept_}, coef: {model.coef_}')
