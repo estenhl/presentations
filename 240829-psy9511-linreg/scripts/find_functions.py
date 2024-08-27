27664b488686e167b4d95a22dd0216b08425df7c
Esten Leonardsen <estenleonardsen@gmail.com>
2024-08-27 19:10:09 +0200

    Progress on linreg section

--- 240829-psy9511-linreg/scripts/find_functions.py
+++ 240829-psy9511-linreg/scripts/find_functions.py
@@ -1,4 +1,6 @@
+
 import os
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -25,3 +27,34 @@ y = df['mpg']
 model = LinearRegression()
 model.fit(X, y)
 print(f'Multiple regression intercept: {model.intercept_}, coef: {model.coef_}')
+
+df['chevy'] = df['name'].apply(lambda x: 1 if 'chevrolet' in x else 0)
+X = df[['horsepower', 'chevy']]
+y = df['mpg']
+model = LinearRegression()
+model.fit(X, y)
+print(f'Interaction term intercept: {model.intercept_}, coef: {model.coef_}')
+
+
+horsepower = np.arange(np.amin(df['horsepower'].values), np.amax(df['horsepower'].values))
+
+chevy_preds = model.predict(np.column_stack((horsepower, np.ones(len(horsepower)))))
+other_preds = model.predict(np.column_stack((horsepower, np.zeros(len(horsepower)))))
+
+plt.plot(horsepower, chevy_preds, c='red')
+plt.plot(horsepower, other_preds, c='blue')
+plt.show()
+
+df['interaction'] = df['horsepower'] * df['chevy']
+X = df[['horsepower', 'chevy', 'interaction']]
+y = df['mpg']
+model = LinearRegression()
+model.fit(X, y)
+print(f'Interaction term intercept: {model.intercept_}, coef: {model.coef_}')
+
+chevy_preds = model.predict(np.column_stack((horsepower, np.ones(len(horsepower)), horsepower)))
+other_preds = model.predict(np.column_stack((horsepower, np.zeros(len(horsepower)), np.zeros(len(horsepower)))))
+
+plt.plot(horsepower, chevy_preds, c='red')
+plt.plot(horsepower, other_preds, c='blue')
+plt.show()
