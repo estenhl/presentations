24d5eaa09067e571fa4eaacea9612ec09d2a4711
Esten Leonardsen <estenleonardsen@gmail.com>
2024-08-28 00:20:32 +0200

    Finished logistic regression part of PSY9511 lecture

--- 240829-psy9511-linreg/scripts/find_functions.py
+++ 240829-psy9511-linreg/scripts/find_functions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 df = pd.read_csv(os.path.join('data', 'Auto.csv'))
 X = df[['horsepower']]
@@ -57,4 +57,20 @@ other_preds = model.predict(np.column_stack((horsepower, np.zeros(len(horsepower
 
 plt.plot(horsepower, chevy_preds, c='red')
 plt.plot(horsepower, other_preds, c='blue')
+plt.show()
+
+X = np.asarray([[36, 25, 26, 32, 28, 29, 29.5, 21,
+                 15, 17, 21, 14, 14.5, 16, 19, 24, 27]]).T
+y = np.asarray([[1, 1, 1, 1, 1, 1, 1, 1,
+                 0, 0, 0, 0, 0, 0, 0, 0, 0]]).T
+model = LinearRegression()
+model.fit(X, y)
+print(f'Linear classification intercept: {model.intercept_}, coef: {model.coef_}')
+
+model = LogisticRegression()
+model.fit(X, y)
+print(f'Logistic classification intercept: {model.intercept_}, coef: {model.coef_}')
+x = np.linspace(10, 40, 100)
+y = model.predict_proba(x.reshape(-1, 1))[:, 1]
+plt.plot(x, y)
 plt.show()
