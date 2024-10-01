7175c76ef13aac7a2c6ecd1dcaa7389e3c306fe7
Esten Leonardsen <estenleonardsen@gmail.com>
2024-10-01 22:04:18 +0200

    Worked on CNN LRP for NAIM2024

--- 241022-nordicai-xai/scripts/create_segmentation.py
+++ 241022-nordicai-xai/scripts/create_segmentation.py
@@ -10,7 +10,7 @@ from skimage.io import imsave
 
 def create_segmentation(input: str):
     mask = nib.load(input).get_fdata()
-    mask = mask[:,128]
+    mask = mask[128]
 
     values, counts = np.unique(mask, return_counts=True)
     print(np.unique(values))
@@ -21,16 +21,15 @@ def create_segmentation(input: str):
 
     for i in range(len(mask)):
         for j in range(len(mask[i])):
-            #rgb[i,j] = cmap(values.index(mask[i,j]) / len(values))[:3]
+            rgb[i,j] = cmap(values.index(mask[i,j]) / len(values))[:3]
 
-            if mask[i,j] in [17, 53]:
-                rgb[i,j] = [255, 0, 0]
+            #if mask[i,j] in [17, 53]:
+            #    rgb[i,j] = [255, 0, 0]
 
 
     rgb[mask == 0] = [0, 0, 0]
     rgb = rgb * 255
     rgb = rgb.astype(np.uint8)
-    rgb = np.rot90(rgb, 3)
 
     plt.imshow(rgb)
     plt.show()
@@ -41,9 +40,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('Creates a segmentation png')
 
     parser.add_argument('--input', required=False,
-                        default=os.path.join('/Applications', 'freesurfer',
-                                             '7.3.1', 'subjects', 'bert',
-                                             'mri', 'aseg.mgz'),
+                        default=os.path.join('/home', 'esten', 'tools',
+                                             'freesurfer-6', 'subjects',
+                                             'bert', 'mri', 'aseg.mgz'),
                         help='Path to input segmentation mask')
 
     args = parser.parse_args()
